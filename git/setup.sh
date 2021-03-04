7ac044e5765a0be8746e39a5a6890478d30744f2
vekio <alberto@casta.me>
2021-03-04 11:40:25 +0100

    Test

--- git/setup.sh
+++ git/setup.sh
@@ -1,7 +1,4 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+#!/bin/bash
 
 # Install git
 sudo apt install git
@@ -9,6 +6,4 @@ sudo apt install git
 # Global config
 git config --global user.name "Alberto Castañeiras"
 git config --global user.email "alberto@casta.me"
-git config --global github.user "vekio"
-git config --global color.ui true
 
