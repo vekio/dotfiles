6d0c762884b305c6cac04412b7725791baea3118
Alberto Castañeiras <alberto@casta.me>
2021-07-10 15:28:19 +0200

    Add make and miniconda

---
+++ sdk/miniconda/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget -P /tmp/ https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+bash /tmp/Miniconda3-latest-Linux-x86_64.sh
+
+source ~/.bashrc
+conda config --set auto_activate_base false
+source ~/.bashrc
