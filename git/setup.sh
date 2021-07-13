754151d5e94ec873504a924a49bc8617d24819ac
Alberto Castañeiras <alberto@casta.me>
2021-07-14 00:29:41 +0200

    Update git

--- git/setup.sh
+++ git/setup.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+# logger
+loginfo() { printf "%b[info]%b %s\n"  '\e[0;32m\033[1m' '\e[0m' "$@" >&2; }
+
+SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) || exit 1
+
+# gitconfig
+ln -sf "$SDIR/gitconfig" ~/.gitconfig
+
 # install
+git --version &>/dev/null && loginfo "git already installed." && exit 0
 sudo apt -y install git
 
 # global config
-git config --global user.name "Alberto Castañeiras"
-git config --global user.email "alberto@casta.me"
+#git config --global user.name "Alberto Castañeiras"
+#git config --global user.email "alberto@casta.me"
