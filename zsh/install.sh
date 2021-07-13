f113fd3c86bb9a0ff6ff681d8714987fac469036
Alberto Castañeiras <alberto@casta.me>
2021-07-13 22:48:00 +0200

    Remove ohmyzsh

--- zsh/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) || exit 1
-
-sudo apt install -y zsh
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+# logger
+loginfo() { printf "%b[info]%b %s\n"  '\e[0;32m\033[1m' '\e[0m' "$@" >&2; }
+logwarn() { printf "%b[warn]%b %s\n"  '\e[0;33m\033[1m' '\e[0m' "$@" >&2; }
+
 SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) || exit 1
 
+# rc files
 ln -sf "$SDIR/zshrc" ~/.zshrc
+ln -sf "$SDIR/zshenv" ~/.zshenv
 
-source ~/.zshrc
+# starship setup
+bash "$SDIR/../cli/starship/setup.sh" || logwarn "starship not installed correctly."
+
+# install zsh
+zsh --version &>/dev/null && loginfo "zsh already installed." && exit 0
+sudo apt install -y zsh &>/dev/null && chsh -s $(which zsh) && exec zsh
