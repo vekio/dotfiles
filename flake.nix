6a2504fc9356c56e726453cd0cd2746e6068087e
alberto <alberto@casta.me>
2025-08-05 20:47:55 +0200

    start new config

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "vekio's dotfiles with home-manager and flakes";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    system = "x86_64-linux";
+    username = "alberto";
+  in {
+    nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./hosts/vm.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.${username} = import ./home/default.nix;
+        }
+      ];
+    };
+  };    
+}
+
---
+++ home/default.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "alberto";
+  home.homeDirectory = "/home/alberto";
+  programs.zsh.enable = true;
+  home.packages = with pkgs; [
+    htop
+    git
+    vim
+  ];
+  home.stateVersion = "25.05";
+}
