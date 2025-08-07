c28dfe32b0158363c53f73eb60f3e92e20736eba
Alberto Castañeiras <alberto@casta.me>
2025-08-07 18:12:34 +0200

    add fzf and aliases

--- home/default.nix
+++ home/default.nix
@@ -19,4 +19,3 @@
     protonvpn-gui
   ];
 }
-
--- modules/cli/zsh.nix
+++ modules/cli/zsh.nix
@@ -1,14 +1,26 @@
 { pkgs, ... }: {
   programs.zsh = {
     enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    autocd = true;
+
     # initContent = ''
     #   function rebuild {
     #     sudo nixos-rebuild switch --flake "$@"
     #   }
     # '';
     shellAliases = {
-      ll = "ls -l";
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      "...." = "cd ../../..";
 
+      ls = "ls --color=auto";
+      la = "ls -lA";
+      ll = "ls -l";
+      l = "ls -A";
+
+      reload = ".. && cd -";
     };
   };
 }
