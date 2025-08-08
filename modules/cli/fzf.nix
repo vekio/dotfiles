b8b2c6887cf95d61fabe31be5889fc6393757e4f
Alberto Castañeiras <alberto@casta.me>
2025-08-08 17:29:21 +0200

    comment cutom fzf

--- modules/cli/fzf.nix
+++ modules/cli/fzf.nix
@@ -1,1 +1,18 @@
-{ programs.fzf = { enable = true; }; }
+{
+  # TODO customize fzf
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+    # defaultOptions =
+    #   [ "--height 40%" "--layout=reverse" "--border" "--inline-info" ];
+    # historyWidgetOptions = [
+    #   "--sort"
+    #   "--exact"
+    #   "--preview-window=down:3:hidden"
+    #   "--bind=?:toggle-preview"
+    #   "--bind=alt-a:select-all,alt-d:deselect-all"
+    # ];
+  };
+
+  # home.packages = with pkgs; [ zsh-forgit zsh-fzf-history-search zsh-fzf-tab ];
+}
