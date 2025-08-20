0ac5b4d8168ac500e24cd2be7387dc25e3dc7e20
Alberto Castañeiras <alberto@casta.me>
2025-08-20 15:13:45 +0200

    add themes source

--- modules/gui/kitty.nix
+++ modules/gui/kitty.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   programs.kitty = {
     enable = true;
+    # https://github.com/kovidgoyal/kitty-themes/tree/master/themes
     themeFile = "Catppuccin-Mocha";
     font = {
       name = "FiraCode Nerd Font";
