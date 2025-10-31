e624d520939e38da2fc11071d2ae0944014124e1
Alberto Castañeiras <alberto@casta.me>
2025-10-31 22:25:37 +0100

    feat: add some fonts

--- hosts/titan/configuration.nix
+++ hosts/titan/configuration.nix
@@ -129,6 +129,12 @@
   # Allow unfree packages
   # nixpkgs.config.allowUnfree = true;
 
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.hack
+  ];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs;
