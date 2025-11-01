316b07f94bf79a65f8d7d8a658916af3b00c7d2d
Alberto Castañeiras <alberto@casta.me>
2025-11-02 00:33:01 +0100

    fix: remove packages.nix

--- modules/cli/default.nix
+++ modules/cli/default.nix
@@ -1,13 +1,6 @@
 { pkgs, ... }: {
-  imports = [
-    ./git.nix
-    ./zsh.nix
-    ./starship.nix
-    ./fzf.nix
-    ./direnv.nix
-    ./tmux.nix
-    ./packages.nix
-  ];
+  imports =
+    [ ./git.nix ./zsh.nix ./starship.nix ./fzf.nix ./direnv.nix ./tmux.nix ];
 
   home.packages = with pkgs; [ gum ];
 }
