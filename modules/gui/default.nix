a41e51df013cce33ebd7efd76a67f44ca6fb85f8
Alberto Castañeiras <alberto@casta.me>
2025-11-02 00:31:26 +0100

    feat: add wl-clipboard

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
   imports = [ ./code.nix ./brave.nix ./xdg.nix ./alacritty.nix ];
 
-  home.packages = with pkgs; [ protonvpn-gui proton-pass vlc ];
+  home.packages = with pkgs; [ protonvpn-gui proton-pass vlc wl-clipboard ];
 }
