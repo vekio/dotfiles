9d551d10a219533af4687fc9a97343ae592bc24d
Alberto Castañeiras <alberto@casta.me>
2025-11-02 00:34:24 +0100

    feat: add spotify

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-  imports = [ ./code.nix ./brave.nix ./xdg.nix ./alacritty.nix ];
+  imports = [ ./code.nix ./brave.nix ./xdg.nix ./alacritty.nix ./spotify.nix ];
 
   home.packages = with pkgs; [ protonvpn-gui proton-pass vlc wl-clipboard ];
 }
