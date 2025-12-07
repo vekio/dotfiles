f5371f8ce2cad4dd5927699f08232edf809f0899
Alberto Castañeiras <alberto@vekio.dev>
2025-12-07 22:02:19 +0100

    add openssl

--- modules/cli/default.nix
+++ modules/cli/default.nix
@@ -2,5 +2,5 @@
   imports =
     [ ./git.nix ./zsh.nix ./starship.nix ./fzf.nix ./direnv.nix ./tmux.nix ];
 
-  home.packages = with pkgs; [ gum vim ];
+  home.packages = with pkgs; [ gum vim openssl ];
 }
