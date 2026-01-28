9eddbc3250bd735d6ff27bd58ce64445d656ae69
Alberto Castañeiras <alberto@vekio.dev>
2026-01-29 00:12:59 +0100

    improve zsh config

---
+++ zsh/aliases.zsh
@@ -0,0 +1,16 @@
+# Home shortcuts
+alias dl='cd ~/Downloads'
+alias docs='cd ~/Documents'
+alias src='cd ~/Source'
+alias dot='cd ~/Source/dotfiles'
+
+# Listing
+alias ll='ls -lh'
+alias la='ls -lha'
+alias l='ls -lh'
+
+# Quick directory jumps
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
---
+++ zsh/options.zsh
@@ -0,0 +1,24 @@
+# History
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+setopt INC_APPEND_HISTORY
+setopt SHARE_HISTORY
+setopt HIST_IGNORE_DUPS
+setopt HIST_REDUCE_BLANKS
+
+# Quality of life
+setopt AUTO_CD           # cd into directories by typing their name
+setopt CORRECT           # minor typo correction for commands
+# setopt CORRECT_ALL     # uncomment if you want correction for args too (can be annoying)
+
+# Completion: case-insensitive
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+
+# Better completion menu
+zstyle ':completion:*' menu select
+zstyle ':completion:*' list-colors ''
+
+# Keybindings (Ctrl+R history search)
+bindkey '^R' history-incremental-search-backward
+
