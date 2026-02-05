83036420f368799afb71617454a1b792c5efb2ab
Alberto Castañeiras <alberto@vekio.dev>
2026-02-05 23:23:39 +0100

    bashrc: add eval brew and clarify zsh auto-switch

---
+++ dot_bashrc.d/10-brew.sh
@@ -0,0 +1,4 @@
+# Load Homebrew environment when brew is installed in the Linuxbrew default path.
+if [ -x /home/linuxbrew/.linuxbrew/bin/brew ]; then
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
--- dot_bashrc.d/20-zsh.sh
+++ dot_bashrc.d/20-zsh.sh
@@ -1,5 +1,5 @@
 # Auto-switch to zsh in interactive shells (useful for toolbox)
+# Only exec zsh when it's available and we have a real terminal.
 if [ -t 1 ] && command -v zsh >/dev/null 2>&1; then
   exec zsh
 fi
-
