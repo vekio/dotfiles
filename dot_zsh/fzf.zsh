7c980f330f5f23270d436104c46b0955a79433ad
Alberto Castañeiras <alberto@vekio.dev>
2026-02-06 00:52:41 +0100

    fzf: fix key bindings path

--- dot_zsh/fzf.zsh
+++ dot_zsh/fzf.zsh
@@ -1,4 +1,11 @@
+fzf_bindings=""
+
 if [ -r /usr/share/fzf/shell/key-bindings.zsh ]; then
-  source /usr/share/fzf/shell/key-bindings.zsh
+  fzf_bindings="/usr/share/fzf/shell/key-bindings.zsh"
+elif [ -r /home/linuxbrew/.linuxbrew/opt/fzf/shell/key-bindings.zsh ]; then
+  fzf_bindings="/home/linuxbrew/.linuxbrew/opt/fzf/shell/key-bindings.zsh"
 fi
 
+if [ -n "$fzf_bindings" ]; then
+  source "$fzf_bindings"
+fi
