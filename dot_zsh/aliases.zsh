6b15b79428fb6a3ca9d3970ce631fc99ccad6d86
Alberto Castañeiras <alberto@vekio.dev>
2026-02-06 17:35:17 +0100

    restore code script wrapper

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -21,8 +21,7 @@ alias la='ls -Ahl'
 # Tooling
 # -----------------------------------------------------------------------------
 # Open chezmoi source in Codium (no cd required).
-alias dotfiles='codium "$(chezmoi source-path)"'
+alias dotfiles='code "$(chezmoi source-path)"'
 
 # Use from toolbox (workaround: flatpak-spawn + alias).
 alias podman='host-podman'
-alias code='flatpak-com.vscodium.codium'
